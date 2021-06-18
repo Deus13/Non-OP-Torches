e2133c3447e055077aaf84641591590519493336
ds5678 <jrpritts7@gmail.com>
2021-06-18 12:54:54 -0400

    Small settings fix

--- src/Settings.cs
+++ src/Settings.cs
@@ -6,13 +6,8 @@ namespace NonOPTorches
     {
         public static Settings instance = new Settings();
 
-        [Name("Breaking Threshhold")]
-        [Description("Torches with lower condition than this threshhold will break on extinguishing. If set to 50% or above, torches form fire will never be able to bet relit just harvested for sticks. (0=Vanilla) ")]
-        [Slider(0, 100f)]
-        public int BreakingThreshhold = 50;
-        
-        [Name("Base Chance lighting")]
-        [Description("Base Chance lighting a torch. The chance will be set to 100% if fire is used to ignite. (100=Vanilla) ")]
+        [Name("Base Chance Lighting")]
+        [Description("Base Chance lighting a torch. The chance will be set to 100% if a fire is used to ignite. (100=Vanilla) ")]
         [Slider(0, 100f)]
         public float BaseChance = 80f;
         
@@ -22,12 +17,17 @@ namespace NonOPTorches
         public float malusCondtion = 2f;
 
         [Name("Chance Bonus for Skill")]
-        [Description("How much the lighting chances are increased for every Skillpiont in Firestarting. (0=Vanilla) ")]
+        [Description("How much the lighting chances are increased for every Skillpoint in Firestarting. (0=Vanilla) ")]
         [Slider(0, 15)]
         public int BonusSkill = 5;
 
-        [Name("Bunrtimebonus")]
-        [Description("Multiplies the burn time . (1=Vanilla) ")]
+        [Name("Breaking Threshold")]
+        [Description("Torches with lower condition than this threshhold will break on extinguishing. If set to 50% or above, torches pulled from fires will never be able to be relit, only harvested for sticks. (0=Vanilla) ")]
+        [Slider(0, 100f)]
+        public int BreakingThreshhold = 50;
+
+        [Name("Burn Time Bonus")]
+        [Description("Multiplies the burn time of torches. (1=Vanilla) ")]
         [Slider(0, 3f)]
         public float burntime = 1.5f;
     }
