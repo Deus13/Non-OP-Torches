20c9838d160b47738b58fd20fe5818a9f34766f3
ds5678 <jrpritts7@gmail.com>
2021-06-18 11:55:08 -0400

    Updated to 2.1.0 for MelonLoader 4

---
+++ src/Settings.cs
@@ -0,0 +1,34 @@
+﻿using ModSettings;
+
+namespace NonOPTorches
+{
+    internal class Settings : JsonModSettings
+    {
+        public static Settings instance = new Settings();
+
+        [Name("Breaking Threshhold")]
+        [Description("Torches with lower condition than this threshhold will break on extinguishing. If set to 50% or above, torches form fire will never be able to bet relit just harvested for sticks. (0=Vanilla) ")]
+        [Slider(0, 100f)]
+        public int BreakingThreshhold = 50;
+        
+        [Name("Base Chance lighting")]
+        [Description("Base Chance lighting a torch. The chance will be set to 100% if fire is used to ignite. (100=Vanilla) ")]
+        [Slider(0, 100f)]
+        public float BaseChance = 80f;
+        
+        [Name("Chance Reduction for Condition")]
+        [Description("How much the lighting chances are reduced by missing condition points of the torch. (0=Vanilla) ")]
+        [Slider(0, 5f)]
+        public float malusCondtion = 2f;
+
+        [Name("Chance Bonus for Skill")]
+        [Description("How much the lighting chances are increased for every Skillpiont in Firestarting. (0=Vanilla) ")]
+        [Slider(0, 15)]
+        public int BonusSkill = 5;
+
+        [Name("Bunrtimebonus")]
+        [Description("Multiplies the burn time . (1=Vanilla) ")]
+        [Slider(0, 3f)]
+        public float burntime = 1.5f;
+    }
+}
